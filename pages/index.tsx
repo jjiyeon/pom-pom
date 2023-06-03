0a2d8c404681bcd441f97725efa0193c2ef44d64
jjiyeon <ejy1105@gmail.com>
2023-06-03 21:39:23 +0900

    fix: delete header component and add contacts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
-import Header from '../components/Header'
 import Main from '../components/Main'
 import Nav from '../components/Nav'
 
 const Home: NextPage = () => {
   return (
     <>
-      <Header />
       <Main />
       <Nav />
     </>
