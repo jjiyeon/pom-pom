c37d69e584a854eede0e8fe442fae50f86103bdf
jjiyeon <ejy1105@gmail.com>
2023-06-03 20:40:29 +0900

    chore: add resources and global style

---
+++ styles/Global.ts
@@ -0,0 +1,43 @@
+import { css } from '@emotion/react'
+
+export const globalStyle = css`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font: inherit;
+    font-family: S-CoreDream;
+    color: inherit;
+    word-wrap: break-word;
+    word-break: keep-all;
+  }
+
+  button {
+    all: unset;
+    cursor: pointer;
+  }
+
+  li,
+  ul {
+    list-style: none;
+  }
+
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    appearance: none;
+    margin: 0;
+  }
+`
